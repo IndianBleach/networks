39e65e0de6b0e5599788e9cdab061ac43fab7551
Boris Zolotov <alabamableach@gmail.com>
2024-08-26 20:41:47 +0300

    fix logging

--- src/http/logging/logger.c
+++ src/http/logging/logger.c
@@ -6,19 +6,19 @@
 #include <time.h>
 
 
-log_err(const char* msg, const char* tag) {
+void log_err(const char* msg, const char* tag) {
     time_t now;
     time(&now);
     printf("[%s ERR] (%s): %s", ctime(&now), tag, msg);
 }
 
-log_warn(const char* msg, const char* tag) {
+void log_warn(const char* msg, const char* tag) {
     time_t now;
     time(&now);
     printf("[%s WRN] (%s): %s", ctime(&now), tag, msg);
 }
 
-log_info(const char* msg, const char* tag) {
+void log_info(const char* msg, const char* tag) {
     time_t now;
     time(&now);
     printf("[%s INFO] (%s): %s", ctime(&now), tag, msg);
--- src/http/main.c
+++ src/http/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "logging/logger.h"
 
 int main() {
     
+    log_err("some error", "global");
     printf("temp\n");
 
     return 0;
