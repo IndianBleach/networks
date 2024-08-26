c927973ebc95c4d1af4fcbd8ff8c9a253dbb0460
Boris Zolotov <alabamableach@gmail.com>
2024-08-27 01:24:53 +0300

    add logger msg endl

--- src/http/logging/logger.c
+++ src/http/logging/logger.c
@@ -9,19 +9,19 @@
 void log_err(const char* msg, const char* tag) {
     time_t now;
     time(&now);
-    printf("[%s ERR] (%s): %s", ctime(&now), tag, msg);
+    printf("[%s ERR] (%s): %s\n", ctime(&now), tag, msg);
 }
 
 void log_warn(const char* msg, const char* tag) {
     time_t now;
     time(&now);
-    printf("[%s WRN] (%s): %s", ctime(&now), tag, msg);
+    printf("[%s WRN] (%s): %s\n", ctime(&now), tag, msg);
 }
 
 void log_info(const char* msg, const char* tag) {
     time_t now;
     time(&now);
-    printf("[%s INFO] (%s): %s", ctime(&now), tag, msg);
+    printf("[%s INFO] (%s): %s\n", ctime(&now), tag, msg);
 }
 
 
