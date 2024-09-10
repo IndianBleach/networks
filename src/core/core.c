92403381c01a6e6708a88dfce82f34d28be08b3f
Boris Zolotov <alabamableach@gmail.com>
2024-09-10 03:36:34 +0300

    core.vector

--- src/core/core.c
+++ src/core/core.c
@@ -1,18 +1,30 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /*
 
-
-    array
-        -at
-        -size
-        -copy
-
     vector
-    
+    -iterator
+        void vector_init(vector *vec, size_t base_cap, size_t type_sz);
+
+        void vector_dstr(vector *vec);
+
+        void vector_ensure_capacity(vector *vec, size_t new_cap);
+
+        void vector_push_back(vector *vec, void *elem);
+
+        void *vector_at(vector *vec, size_t index);
+
+        void vector_resize(vector *vec, size_t new_size);
+
+        void vector_shrink_to_fit(vector *vec);
+
+        void vector_clear(vector *vec);
+
+        void vector_reserve(vector *vec, size_t count);
 
     array
     -iterator()
@@ -21,7 +33,7 @@
 
 
 void log(FILE *fd, const char *__msg, const char *__caller, const char *__file, const int __line) {
-    fprintf(fd, "[ERR: %s:%i <%s>]: %s\n", __file, __line, __caller, __msg);
+    fprintf(fd, "[%s:%i <%s>]: %s\n", __file, __line, __caller, __msg);
 }
 
 #define __log_err(msg) log(stderr, msg, __func__, __FILE__, __LINE__);
@@ -82,6 +94,11 @@ void *iter_back(basic_iterator *it) {
     }
 }
 
+void *iter_realloc(basic_iterator *it, size_t _newcapacity) {
+    it->begin = (void *) realloc(it->begin, (it->diff * _newcapacity));
+    it->len = _newcapacity;
+}
+
 #pragma endregion
 
 // ::::::: Containers
@@ -120,19 +137,124 @@ void array_set(array *arr, size_t __index, void *__value) {
     }
 }
 
+void array_dstr(array *arr) { free(arr->iterator.begin); }
+
 #pragma endregion
+
+#pragma region Vector
+
+typedef struct vector {
+    size_t size;
+    const basic_iterator iterator;
+    // size + dynamic memory blocks
+} vector;
+
+#define __vector_at(VEC, TYPE, INDEX) (TYPE *) vector_at(VEC, INDEX)
+#define __vector_first(VEC, TYPE) (TYPE *) vector_first(VEC)
+#define __vector_last(VEC, TYPE) (TYPE *) vector_last(VEC)
+
+void vector_init(vector *vec, size_t __basecap, size_t __elem_sz) {
+    void *begin = malloc(__elem_sz * __basecap);
+    iter_init(&vec->iterator, begin, __elem_sz, __basecap);
+    vec->size = 0;
+}
+
+void vector_dstr(vector *vec) { free(vec->iterator.begin); }
+
+void vector_ensure_capacity(vector *vec, size_t __new_cap) { iter_realloc(&vec->iterator, vec->iterator.len * 2); }
+
+void vector_pushback(vector *vec, void *__value) {
+    if (__value == NULL) {
+        return;
+    }
+
+    // cap=count allocated links
+    if (vec->size == vec->iterator.len) {
+        vector_ensure_capacity(vec, vec->iterator.len * 2);
+    }
+
+    iter_set(&vec->iterator, vec->size);
+    void *ic = iter_cur(&(vec->iterator));
+
+    memcpy(ic, __value, vec->iterator.diff);
+    vec->size++;
+}
+
+void *vector_at(vector *vec, size_t __index) {
+    if (__index > vec->size) {
+        return NULL;
+    }
+
+    iter_set(&vec->iterator, __index);
+    return iter_cur(&vec->iterator);
+
+    //return vec->buff + (sizeof(void *) * index);
+}
+
+void *vector_last(vector *vec) {
+    if (vec->size == 0)
+        return NULL;
+    else {
+        iter_set(&vec->iterator, vec->size - 1);
+        return iter_cur(&vec->iterator);
+    }
+}
+
+void *vector_first(vector *vec) {
+    if (vec->size == 0)
+        return NULL;
+    else
+        return vec->iterator.begin;
+}
+
+void vector_resize(vector *vec, size_t __newsz) { vec->size = __newsz; }
+
+void vector_shrink_to_fit(vector *vec) {
+    // delete elements by capacity
+    //size_t new_size = sizeof(void *) * vec->size;
+
+    iter_realloc(&vec->iterator, vec->size);
+}
+
+void vector_clear(vector *vec) {
+    vector_resize(vec, 0);
+    vector_shrink_to_fit(vec);
+}
+
+void vector_reserve(vector *vec, size_t count) {
+    size_t free_now = (vec->iterator.len - vec->size);
+
+    if (free_now < count) {
+        vector_ensure_capacity(vec, count);
+    }
+}
+
+#pragma endregion
+
+// rm
 
 int main() {
     printf("HI!\n");
 
     int v1 = 5;
+    int v2 = 9;
+    int v3 = -20;
 
-    array a;
-    array_init(&a, 4, sizeof(int));
-    array_set(&a, 3, &v1);
+    vector v;
+    vector_init(&v, 2, sizeof(int));
 
-    int cur = *__array_at(&a, int, 3);
-    printf("at=%i\n", cur);
+    void *end = iter_end(&v.iterator);
+    void *s = iter_begin(&v.iterator);
+
+    printf("s=%p end=%p\n", s, end);
+
+    vector_pushback(&v, &v1);
+    vector_pushback(&v, &v2);
+
+    int *cast = __vector_last(&v, int);
+    printf("cast=%i\n", *cast);
+
+    vector_dstr(&v);
 
     return 0;
 }
